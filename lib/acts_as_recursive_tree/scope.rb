a76f864c8eaf2c19201fcb4c95a77768825250b7
Wolfgang Wedelich-John <wolfgang.wedelich@1und1.de>
2015-05-18 11:17:27 +0200

    replaced hardcoded tablename by config value

--- lib/acts_as_recursive_tree/scope.rb
+++ lib/acts_as_recursive_tree/scope.rb
@@ -8,11 +8,11 @@ module ActsAsRecursiveTree
       scope :without, ->(record) { where.not(id: record.id) }
 
       scope :ancestors_of, ->(record) {
-        where("physical_units.id in(#{related_recursive_items(record, descendants: false, arel: true, only_id: true).to_sql})")
+        where("#{recursive_tree_config[:base_class].constantize.table_name}.id in(#{related_recursive_items(record, descendants: false, arel: true, only_id: true).to_sql})")
       }
 
       scope :descendants_of, ->(record) {
-        where("physical_units.id in(#{related_recursive_items(record, arel: true, only_id: true).to_sql})")
+        where("#{recursive_tree_config[:base_class].constantize.table_name}.id in(#{related_recursive_items(record, arel: true, only_id: true).to_sql})")
       }
 
     end
