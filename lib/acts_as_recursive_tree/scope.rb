408b965d5b549bb4c6d9ee5faf6cea5b1a82ca56
Wolfgang Wedelich-John <wolfgang.wedelich@1und1.de>
2015-07-23 10:54:49 +0200

    proper scope naming and additional scopes

--- lib/acts_as_recursive_tree/scope.rb
+++ lib/acts_as_recursive_tree/scope.rb
@@ -5,14 +5,23 @@ module ActsAsRecursiveTree
     included do
       scope :roots, -> { where(recursive_tree_config[:foreign_key] => nil) }
 
-      scope :without, ->(record) { where.not(id: record.id) }
+      scope :without, ->(record) { where.not(id: record.is_a?(ActiveRecord::Base) ? record.id : record) }
 
-      scope :ancestors_of, ->(record) {
+
+      scope :self_and_ancestors_of, ->(record) {
         where("#{recursive_tree_config[:base_class].constantize.table_name}.id in(#{related_recursive_items(record, descendants: false, arel: true, only_id: true).to_sql})")
       }
 
-      scope :descendants_of, ->(record) {
+      scope :ancestors_of, ->(record) {
+        self_and_ancestors_of(record).without(record)
+      }
+
+      scope :self_and_descendants_of, ->(record) {
         where("#{recursive_tree_config[:base_class].constantize.table_name}.id in(#{related_recursive_items(record, arel: true, only_id: true).to_sql})")
+      }
+
+      scope :descendants_of, ->(record) {
+        self_and_descendants_of(record).without(record)
       }
 
     end
