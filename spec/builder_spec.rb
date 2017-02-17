49da1153aa3564aec18b254bb85aa9d8c9c0ff41
Wolfgang Wedelich-John <wolfgang.wedelich@1und1.de>
2017-02-17 16:20:03 +0100

    added more tests

--- spec/builder_spec.rb
+++ spec/builder_spec.rb
@@ -5,7 +5,8 @@ shared_examples 'basic recursive examples' do
   let(:builder) { described_class.new(model_class, model_id) }
   subject(:query) { builder.build.to_sql }
 
-  it { is_expected.to start_with "SELECT \"#{model_class.table_name}\".* FROM \"#{model_class.table_name}\""}
+  it { is_expected.to start_with "SELECT \"#{model_class.table_name}\".* FROM \"#{model_class.table_name}\"" }
+  it { is_expected.to match /WHERE "#{model_class.table_name}"."#{model_class.primary_key}" = #{model_id}/ }
   it { is_expected.to match /WITH RECURSIVE "#{builder.travers_loc_table.name}" AS/ }
   it { is_expected.to match /SELECT "#{model_class.table_name}"."#{model_class.primary_key}", "#{model_class.table_name}"."#{model_class._recursive_tree_config.parent_key}", 0 AS recursive_depth FROM "#{model_class.table_name}"/ }
   it { is_expected.to match /SELECT "#{model_class.table_name}"."#{model_class.primary_key}", "#{model_class.table_name}"."#{model_class._recursive_tree_config.parent_key}", \("#{builder.travers_loc_table.name}"."recursive_depth" \+ 1\) AS recursive_depth FROM "#{model_class.table_name}"/ }
@@ -28,34 +29,37 @@ shared_examples 'build recursive query' do
       let(:model_class) { Floor }
     end
   end
+end
 
+shared_examples 'ancestor query' do
+  let(:model_id) { 1 }
+  let(:model_class) { Node }
+  let(:builder) { described_class.new(model_class, model_id) }
+  subject(:query) { builder.build.to_sql }
 
-  context 'other' do
+  it { is_expected.to match /"#{builder.travers_loc_table.name}"."#{model_class._recursive_tree_config.parent_key}" = "#{model_class.table_name}"."#{model_class.primary_key}"/ }
+end
 
+shared_examples 'descendant query' do
+  let(:model_id) { 1 }
+  let(:model_class) { Node }
+  let(:builder) { described_class.new(model_class, model_id) }
+  subject(:query) { builder.build.to_sql }
 
-    let(:model_class) { Node }
-    let(:model_id) { 1 }
-    let(:builder) { described_class.new(model_class, model_id) }
-    subject(:query) { builder.build.to_sql }
-
-    it 'should work' do
-      puts query
-    end
-
-
-    it { is_expected.to match /WHERE "#{model_class.table_name}"."#{model_class.primary_key}" = #{model_id}/ }
-  end
-
+  it { is_expected.to match /"#{model_class.table_name}"."#{model_class._recursive_tree_config.parent_key}" = "#{builder.travers_loc_table.name}"."#{model_class.primary_key}"/ }
 end
 
 describe ActsAsRecursiveTree::Builder::Descendants do
   it_behaves_like 'build recursive query'
+  it_behaves_like 'descendant query'
 end
 
 describe ActsAsRecursiveTree::Builder::Ancestors do
   it_behaves_like 'build recursive query'
+  it_behaves_like 'ancestor query'
 end
 
 describe ActsAsRecursiveTree::Builder::Leaves do
   it_behaves_like 'build recursive query'
+  it_behaves_like 'descendant query'
 end
