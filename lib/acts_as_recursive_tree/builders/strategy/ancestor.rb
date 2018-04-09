e4a572c757b88f9522745d0189b712d85b6c2d04
Wolfgang Wedelich-John <wolfgang.wedelich@1und1.de>
2018-04-09 10:14:40 +0200

    added doc

--- lib/acts_as_recursive_tree/builders/strategy/ancestor.rb
+++ lib/acts_as_recursive_tree/builders/strategy/ancestor.rb
@@ -1,7 +1,13 @@
 module ActsAsRecursiveTree
   module Builders
     module Strategy
+      #
+      # Strategy for building ancestors relation
+      #
       module Ancestor
+        #
+        # Builds the relation
+        #
         def self.build(builder)
           builder.travers_loc_table[builder.parent_key].eq(builder.base_table[builder.primary_key])
         end
--- lib/acts_as_recursive_tree/builders/strategy/descendant.rb
+++ lib/acts_as_recursive_tree/builders/strategy/descendant.rb
@@ -1,7 +1,13 @@
 module ActsAsRecursiveTree
   module Builders
     module Strategy
+      #
+      # Strategy for building descendants relation
+      #
       module Descendant
+        #
+        # Builds the relation
+        #
         def self.build(builder)
           builder.base_table[builder.parent_key].eq(builder.travers_loc_table[builder.primary_key])
         end
