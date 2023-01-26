07427b86c679754b3d777ab663ccde3b227c20cf
Wolfgang Wedelich-John <wolfgang.wedelich@ionos.com>
2023-01-26 13:38:42 +0100

    acts_as_recursive_tree-26 Add include options for preload_tree

--- spec/support/tree_methods.rb
+++ spec/support/tree_methods.rb
@@ -2,11 +2,12 @@
 
 # Helper methods for simple tree creation
 module TreeMethods
-  def create_tree(max_level, current_level = 0, node = nil)
+  def create_tree(max_level, current_level = 0, node = nil, create_node_info: false)
     node = Node.create!(name: 'root') if node.nil?
 
     1.upto(max_level - current_level) do |index|
       child = node.children.create!(name: "child #{index} - level #{current_level}")
+      child.create_node_info if create_node_info
       create_tree(max_level, current_level + 1, child)
     end
 
